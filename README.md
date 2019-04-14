aecb8fde92e116fd228b1fa3d195da299cb0551d
Kevin Weaver <kevinweaver2@gmail.com>
2019-04-14 16:50:32 -0700

    Add gitignore

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # kiln-ethereum
 
 The smart contracts that power [Kiln](https://github.com/FreekingDean/kiln).
+
+# Developer Setup
+
+1. Install Truffle: `npm install -g truffle`
+1. Install [Ganach](https://truffleframework.com/ganache) for a local blockchain instance.
+1. Compile the smart contracts: `truffle compile`
---
+++ contracts/TreasureToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract TreasureToken is ERC20, ERC20Detailed {
+  string public constant NAME = 'TreasureToken';
+  string public constant SYMBOL = 'TRSR';
+  uint8 public constant DECIMALS = 18;
+  uint constant INITIAL_SUPPLY = 100000 * (10 ** uint256(DECIMALS));
+
+  constructor () public ERC20Detailed(NAME, SYMBOL, DECIMALS){
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
+}
