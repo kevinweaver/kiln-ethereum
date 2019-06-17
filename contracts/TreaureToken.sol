35bd7fc8e223f88e707fa9a04a3aa4d7c588773b
Kevin Weaver <kevinweaver2@gmail.com>
2019-06-16 20:58:58 -0700

    Update initial supply and decimals on treasure token

--- contracts/TreaureToken.sol
+++ contracts/TreaureToken.sol
@@ -5,8 +5,8 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 contract TreasureToken is ERC20 {
     string public name = "TreasureToken";
     string public symbol = "TT";
-    uint public decimals = 2;
-    uint public INITIAL_SUPPLY = 12000;
+    uint public decimals = 10;
+    uint public INITIAL_SUPPLY = 10_000_000;
 
     constructor() public {
         _mint(msg.sender, INITIAL_SUPPLY);
